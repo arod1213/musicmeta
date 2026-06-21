8310e064132a5ccea0d15f3d2185cefe1a560f03
arod1213 <aidan@aidanrodriguez.com>
2026-06-21 15:41:00 -0700

    add isrc from str

--- src/isrc/mod.rs
+++ src/isrc/mod.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
 
 use serde::{Deserialize, Serialize};
 
@@ -22,11 +22,30 @@ impl Display for Isrc {
 
 // CC-XXX-YY-NNNNN
 // country, registrant, year, serial
-// impl Isrc {
-//     fn clean(x: &str) -> String {
-//         x.replace(" ", "")
-//             .replace("-", "")
-//             .to_uppercase()
-//             .to_string()
-//     }
-// }
+fn clean_isrc(x: &str) -> String {
+    x.replace(" ", "")
+        .replace("-", "")
+        .to_uppercase()
+        .to_string()
+}
+
+impl FromStr for Isrc {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let clean = clean_isrc(s);
+        if clean.len() != 11 {
+            return Err(());
+        }
+        let country = clean[0..2].to_string();
+        let creator = clean[2..5].to_string();
+        let year_suffix = clean[5..7].parse::<u8>().map_err(|_| ())?;
+        let id = clean[7..].parse::<u32>().map_err(|_| ())?;
+        Ok(Isrc {
+            country,
+            creator,
+            year_suffix,
+            id,
+        })
+    }
+}
